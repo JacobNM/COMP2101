b48c599fa53c3a84a09b74365996da04dccc00bd
Jacob <j@cobmart.in>
2023-06-01 10:34:51 -0400

    System Info tool - First Lab

---
+++ bash/sysinfo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Script is designed to provide output of computer information
+
+# Searches for PC hostname ip address and prints it out, followed by new-line character
+printf "FQDN: $(hostname --fqdn)\n"
+
+# Provides hostname information
+printf "Host Information:\n $(hostnamectl)\n"
+
+# Prints out available IP-addresses for host, not including 127 networks
+printf "IP Addresses:\n $(hostname -I)\n"
+
+# checks space in only root system, displayed as human-friendly text output
+printf "Root Filesystem Access:\n"  
+df / -h
+
