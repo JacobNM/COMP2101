798603b3d789351b88ffb46a1ca84389a2e22749
Jacob <j@cobmart.in>
2023-06-02 19:40:02 -0400

    Tweaks to hostname information (grep)

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -5,12 +5,12 @@
 # Searches for PC hostname 
 # Provides hostname information
 # Prints out available IP-addresses for host, not including 127 networks
-# checks space in only root system, displayed as human-friendly text output
+# checks available space in only root system, displayed as human-friendly text output
 cat <<EOF
 Current VM Information
 ======================
 FQDN: $(hostname --fqdn)
-Operating System and version: $(hostnamectl | head -n 7 | tail -n 1 | sed 's/Operating System: //')
+$(hostnamectl | grep -w "Operating System" | sed 's/Operating System:/Operating System and version:/')
 IP Address: $(hostname -I)
 Root Filesystem Space remaining:$(df -h -t ext4 --output=avail | tail -1) 
 ======================
