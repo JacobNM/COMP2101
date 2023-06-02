29b9f1cadacff87fb6544ae89be00b343e2522d3
Jacob <j@cobmart.in>
2023-06-01 20:16:52 -0400

    Slight modifications to printf arguments

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,14 +2,14 @@
 # Script is designed to provide output of computer information
 
 # Searches for PC hostname ip address and prints it out, followed by new-line character
-echo 'FQDN: $(hostname --f)'
+echo "FQDN: $(hostname -f)"
 
 # Provides hostname information
-printf "Host Information:\n $(hostnamectl)\n"
+printf "Host Information: \n%s\n" "$(hostnamectl)"
 
 # Prints out available IP-addresses for host, not including 127 networks
-printf "IP Addresses:\n $(hostname -I)\n"
+printf "IP Addresses: \n%s\n" "$(hostname -I)"
 
 # checks space in only root system, displayed as human-friendly text output
-printf "Root Filesystem Access:\n  $(df / -h)"
+printf "Root Filesystem Access: \n%s\n"  "$(df / -h)"
 
