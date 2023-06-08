13e12febe9c2a526611c473cdae7a8d7497709d8
Jacob <j@cobmart.in>
2023-06-08 12:51:00 -0400

    Lab 3 - Minor adjustments

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -4,11 +4,15 @@
 ## Checks is user is root; if not, prompts them to use sudo for commands
 # if [ "$(whoami)" != "root" ]; then echo "must be root (try sudo at beginning of command)";exit 1; fi
 
+
+source /etc/os-release
+
 # Variable list
 MYFQDN=$(hostname -f)
 Operating_System_And_Version=$(hostnamectl | grep -w "Operating System" | sed 's/Operating System:/Operating System and version:/')
 My_IP=$(hostname -I)
 Root_FileSystem_Space=$(df -h -t ext4 --output=avail | tail -1) 
+My_Computer_Vendor=$(sudo lshw | grep -w "vendor" | head -n 1 | sed 's/    vendor: //')
 
 # Searches for PC hostname 
 # Provides hostname information
@@ -19,6 +23,12 @@ Root_FileSystem_Space=$(df -h -t ext4 --output=avail | tail -1)
 cat <<EOF
 
 System info produced by $USER
+
+
+System Description
+==================
+Vendor: $My_Computer_Vendor
+
 
 Current VM Information
 ======================
