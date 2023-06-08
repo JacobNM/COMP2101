e4161ea8b154bb09640576f0cc2649e84c6e2caf
Jacob <j@cobmart.in>
2023-06-08 12:22:54 -0400

    Added root privilege Req. to run script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 # Script is designed to provide output of computer information
 
+## Checks is user is root; if not, prompts them to use sudo for commands
+# if [ "$(whoami)" != "root" ]; then echo "must be root (try sudo at beginning of command)";exit 1; fi
+
+# Variable list
+MYFQDN=$(hostname -f)
+Operating_System_And_Version=$(hostnamectl | grep -w "Operating System" | sed 's/Operating System:/Operating System and version:/')
+My_IP=$(hostname -I)
+Root_FileSystem_Space=$(df -h -t ext4 --output=avail | tail -1) 
 
 # Searches for PC hostname 
 # Provides hostname information
 # Prints out available IP-addresses for host, not including 127 networks
 # checks available space in only root system, displayed as human-friendly text output
+# Provided in template form using cat
+
 cat <<EOF
+
+System info produced by $USER
+
 Current VM Information
 ======================
-FQDN: $(hostname --fqdn)
-$(hostnamectl | grep -w "Operating System" | sed 's/Operating System:/Operating System and version:/')
-IP Address: $(hostname -I)
-Root Filesystem Space remaining:$(df -h -t ext4 --output=avail | tail -1) 
+FQDN: $MYFQDN
+$Operating_System_And_Version
+IP Address:$My_IP 
+Root Filesystem Space remaining:$Root_FileSystem_Space
 ======================
 
 EOF
