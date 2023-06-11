ca29cc24e5740d343830093e09549d932c680430
Jacob <j@cobmart.in>
2023-06-11 00:52:06 -0400

    Small changes; CPU almost done

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -7,9 +7,16 @@
 # Grabs operating system release file for utilization of variables contained within
 source /etc/os-release
 
-# Variable list
+## Variable list
+# System variables
 My_Computer_Vendor=$(sudo lshw | grep -w "vendor" | head -n 1 | sed 's/ *vendor: //')
 Computer_Serial_Numer=$(sudo lshw | grep -w "serial:" | head -n 1 | sed 's/ *serial: //')
+
+# CPU variables
+CPU_Manufacturer=$(sudo dmidecode -s processor-manufacturer | head -n 1)
+CPU_Architecture=$(hostnamectl | grep Architecture | sed 's/  *Architecture: //')
+CPU_Max_Speed=$(sudo dmidecode -t processor | grep Hz | head -n 1 | sed 's/ *Max Speed://')
+# Operating System variables
 MY_FQDN=$(hostname -f)
 My_IP=$(hostname -I)
 Root_FileSystem_Space=$(df -h -t ext4 --output=avail | tail -1 | sed 's/  *//') 
@@ -35,13 +42,15 @@ Computer Serial Number:          $Computer_Serial_Numer
 
 CPU Information
 ===============
-
+CPU Manufacturer/Model:          $CPU_Manufacturer
+CPU Architecture:                $CPU_Architecture
+CPU Max Speed:                 $CPU_Max_Speed
 
 Current VM Information
 ======================
 FQDN:                            $MY_FQDN
 IP Address:                      $My_IP
-Root Filesystem Space remaining: $Root_FileSystem_Space
+Root Filesystem Space Remaining: $Root_FileSystem_Space
 ======================
 
 EOF
