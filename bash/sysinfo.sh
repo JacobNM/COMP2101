b5f3d07eb9c14d8950e6d7d411b561cfb59e9b02
Jacob <j@cobmart.in>
2023-06-11 19:55:23 -0400

    Lab 3 done

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,49 +2,41 @@
 # Script is designed to provide output of computer information
 
 ## Checks is user is root; if not, prompts them to use sudo for commands
-# if [ "$(whoami)" != "root" ]; then echo "must be root (try sudo at beginning of command)";exit 1; fi
+# if [ "$(whoami)" != "root" ]; then echo "must be root (try "sudo" at beginning of script command)";exit 1; fi
 
 # Grabs operating system release file for utilization of variables contained within
 source /etc/os-release
 
 ## Variable list
-# System variables
-My_Computer_Vendor=$(sudo lshw | grep -w "vendor" | head -n 1 | sed 's/ *vendor: //')
-Computer_Serial_Numer=$(sudo lshw | grep -w "serial:" | head -n 1 | sed 's/ *serial: //')
 
-# CPU variables
-CPU_Manufacturer=$(sudo dmidecode -s processor-manufacturer | head -n 1)
-CPU_Architecture=$(hostnamectl | grep Architecture | sed 's/  *Architecture: //')
-CPU_Max_Speed=$(sudo dmidecode -t processor | grep Hz | head -n 1 | sed 's/ *Max Speed://')
-# Operating System variables
+# Basic Info variables
+Current_Time=$(date +"%I:%M%p %Z")
 MY_FQDN=$(hostname -f)
 My_IP=$(hostname -I)
 Root_FileSystem_Space=$(df -h -t ext4 --output=avail | tail -1 | sed 's/  *//') 
-#CPU_manufacturer=$(sudo lshw | grep -w -A 12 "cpu:0")  << NOT FINISHED
 
-# Searches for PC hostname 
-# Provides hostname information
-# Prints out available IP-addresses for host, not including 127 networks
-# checks available space in only root system, displayed as human-friendly text output
-# Provided in template form using cat
+# System variables
+Computer_Manufacturer=$(sudo dmidecode -s system-manufacturer)
+Computer_Model=$(sudo lshw -C system | grep -w "product" | head -n 1 | sed 's/ *product: //')
+Computer_Serial_Numer=$(sudo lshw | grep -w "serial:" | head -n 1 | sed 's/ *serial: //')
+
+# CPU variables
+CPU_Manufacturer=$(sudo lshw -C processor | grep -w "product" | head -n 1 | sed 's/ *product: //')
+CPU_Architecture=$(hostnamectl | grep Architecture | sed 's/  *Architecture: //')
+CPU_Max_Speed=$(sudo dmidecode -t processor | grep Hz | head -n 1 | sed 's/ *Max Speed://')
+CPU_Total_Cores=$(( $(lscpu | awk '/^Socket\(s\)/{ print $2 }') * $(lscpu | awk '/^Core\(s\) per socket/{ print $4 }') ))
+CPU_L1_Cache_Size=$(sudo lscpu --caches=NAME,ONE-SIZE | grep L1 | sed 's/K/KBs/' | sed '2 s/L1/                                 L1/')
+CPU_L2_Cache_Size=$(sudo lscpu --caches=NAME,ONE-SIZE | grep L2 | sed 's/K/KBs/')
+CPU_L3_Cache_Size=$(sudo lscpu --caches=NAME,ONE-SIZE | grep L3 | sed 's/M/MBs/' )
+
+
+# Script will Search for PC hostname, print available IP addresses of host (not including 127 networks)
+# checks available space in root system, displayed as human-friendly text output
+# Provided in template form using cat command
 
 cat <<EOF
 
-System info produced by $USER
-
-
-System Description
-==================
-Manufacturer/Vendor:             $My_Computer_Vendor
-Operating System And Version:    $PRETTY_NAME
-Computer Serial Number:          $Computer_Serial_Numer
-==================
-
-CPU Information
-===============
-CPU Manufacturer/Model:          $CPU_Manufacturer
-CPU Architecture:                $CPU_Architecture
-CPU Max Speed:                 $CPU_Max_Speed
+System info produced by $USER at $Current_Time
 
 Current VM Information
 ======================
@@ -52,5 +44,29 @@ FQDN:                            $MY_FQDN
 IP Address:                      $My_IP
 Root Filesystem Space Remaining: $Root_FileSystem_Space
 ======================
+
+System Description
+==================
+Manufacturer/Vendor:             $Computer_Manufacturer
+Computer Description:            $Computer_Model
+Computer Serial Number:          $Computer_Serial_Numer
+==================
+
+CPU Information
+===============
+CPU Manufacturer/Model:          $CPU_Manufacturer
+CPU Architecture:                $CPU_Architecture
+CPU Core Total:                  $CPU_Total_Cores
+CPU Max Speed:                 $CPU_Max_Speed
+CPU L1 Cache Size:               $CPU_L1_Cache_Size
+CPU L2 Cache Size:               $CPU_L2_Cache_Size
+CPU L3 Cache Size:               $CPU_L3_Cache_Size
+===============
+
+Operating System Information
+============================
+Operating System:                $NAME
+Version:                         $VERSION
+============================
 
 EOF
