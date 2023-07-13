bd2333c91849fe55a82dbf3ecd3d18e0aea658fa
JacobNM <j@cobmart.in>
2023-07-13 18:57:44 -0400

    Updated to welcome message w/ date

--- Powershell/Helloworld.ps1
+++ Powershell/Helloworld.ps1
@@ -1,2 +1,31 @@
 $todaysDate = $(get-date -UFormat "%A %B %d %Y")
-write-host "Hello World! Todays date is $($todaysDate)"
+
+if ((Get-Date).DayOfWeek -eq "Sunday") {
+    $Salutation = "Sir"
+}
+
+elseif ((Get-Date).DayOfWeek -eq "Monday") {
+    $Salutation = "Private"
+}
+
+elseif ((Get-Date).DayOfWeek -eq "Tuesday") {
+    $Salutation = "Master Chief"
+}
+
+elseif ((Get-Date).DayOfWeek -eq "Wednesday") {
+    $Salutation = "Monsieur"
+}
+
+elseif ((Get-Date).DayOfWeek -eq "Thursday") {
+    $Salutation = "Commander"
+}
+
+elseif ((Get-Date).DayOfWeek -eq "Friday") {
+    $Salutation = "Colonel"
+}
+
+elseif ((Get-Date).DayOfWeek -eq "Saturday") {
+    $Salutation = "Wingman"
+}
+
+Write-Host "Hello $($Salutation)! Todays date is $($todaysDate)"
